4c9aa9c2a35c5777f86948fdc199776fccc7ce00
bokChoy_Linux <amychoi7@snu.ac.kr>
2024-01-19 12:30:50 +0900

    add FeScu MOLDEN

--- pyXRAYsimul/slurm.sh
+++ pyXRAYsimul/slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 # 
-#SBATCH --job-name=06_XAS
-#SBATCH --output=06_slurmXAS.out
+#SBATCH --job-name=03_XAS
+#SBATCH --output=03_slurmXAS.out
 #SBATCH --nodelist=node5
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=36
@@ -12,4 +12,4 @@ conda activate pyscf
 echo $CONDA_PREFIX
 
 export OMP_NUM_THREADS=36
-python 06_FeSdi_xas.py > 06_FeSdi_xas.out
+python 03_FeSdi_xas.py > 03_FeSdi_xas.out
