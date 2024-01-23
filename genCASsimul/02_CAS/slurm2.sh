7d93d2f2b64ea808e9ba84797be4856d7a0bf954
bokChoy_Linux <amychoi7@snu.ac.kr>
2024-01-23 15:45:58 +0900

    add 02_CAS: simul for FeScu

---
+++ genCASsimul/02_CAS/slurm2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -i
+# 
+#SBATCH --job-name=01_loc
+#SBATCH --output=slurm1.out
+#SBATCH --nodelist=node3
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=24:00:00
+
+source ~/.bashrc
+conda activate pyscf
+echo $CONDA_PREFIX
+
+mysimulstr='01'
+
+export OMP_NUM_THREADS=36
+python nlocal.py > nlocal.out
